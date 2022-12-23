35ef451164e0572068b339b572e8f0999103c961
Angel Camargo <adcamargo10@gmail.com>
2022-12-23 17:11:02 -0400

    First Commit

---
+++ assets/javascript/script.js
@@ -0,0 +1,26 @@
+// page navigation variables
+const navigationLinks = document.querySelectorAll("[data-nav-link]");
+const pages = document.querySelectorAll("[data-page]");
+
+navigationLinks.forEach(navigationLink => {
+  navigationLink.addEventListener('click', function(e) {
+    e.preventDefault();
+  });
+});
+
+// add event to all nav link
+for (let i = 0; i < navigationLinks.length; i++) {
+  navigationLinks[i].addEventListener("click", function () {
+
+    for (let i = 0; i < pages.length; i++) {
+      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
+        pages[i].classList.add("active");
+        navigationLinks[i].classList.add("active");
+      } else {
+        pages[i].classList.remove("active");
+        navigationLinks[i].classList.remove("active");
+      }
+    }
+
+  });
+}
